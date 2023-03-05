e4b518b5f38f371b23a4f933b6ce1d67bb61336d
PSY222 <ss26348@naver.com>
2023-03-05 14:19:48 +0900

    sliding window

--- deeplearning_pracitce/PSPNet_practice.py
+++ deeplearning_pracitce/PSPNet_practice.py
@@ -153,7 +153,38 @@ class PyramidPooling(nn.Module):
         output = torch.cat([x,out1,out2,out3,out4],dim=1)
         return output
 
-        
+class DecodePSPFeature(nn.Module):
+    def __init__(self,height,width,n_classes):
+        super(DecodePSPFeature,self).__init__()
+        self.height=height
+        self.width = width
+
+        self.cbr = conv2DBatchNormRelu(in_channels=4096, out_channels =512, kernel_size=3, stride=1, padding=1, dilation=1, bias=False )
+        self.dropout = nn.Dropout2d(p=0.1)
+        self.classification = nn.Conv2d(in_channels=512, out_channels=n_classes, kernel_size=1,stride=1, padding=0)
+
+    def forward(self,x):
+        x = self.cbr(x)
+        x = self.dropout(x)
+        x = self.classification(x)
+        output = F.interpolate(x, size=(self.height,self.width), mode= "bilinear",align_corners=True)
+        return output
+
+class AuxiliaryPSPlayers(nn.Module):
+    def __init__(self, in_channels, height, width, n_classes):
+        super(AuxiliaryPSPlayers,self).__init__()
+
+        self.height= height
+        self.width = width
+        self.cbr = conv2DBatchNormRelu(in_channels=in_channels, out_channels=256, kernel_size=3, stride=1, padding=1, dilation=1,bias=False) 
+        self.dropout = nn.Dropout2d(p=0.1)
+        self.classification = nn.Conv2d(in_channels=256,out_channels=n_classes, kernel_size=1, stride=1,padding=0)
+
+    def forward(self,x):
+        x = self.cbr(x)
+        x = self.dropout(x)
+        output = F.interpolate(x, size=(self.height,self.width), mode = "bilinear", align_corners=True)
+        return output       
 
         
 
